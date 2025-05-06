5af87805e2f27e7443ad906f4c8b34937d9f9c74
minju <sfhketum1231@gmail.com>
2025-05-06 15:54:14 +0900

    git commit "회원가입 화면 구성"

--- app/src/main/java/com/example/mobileteamapp/NewMemberActivity.java
+++ app/src/main/java/com/example/mobileteamapp/NewMemberActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileteamapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewMemberActivity extends AppCompatActivity {
@@ -8,5 +11,11 @@ public class NewMemberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_member); // new_member.xml로 연결
+
+        Button nextPageBtn = (Button) findViewById(R.id.next_page_1);
+        nextPageBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(NewMemberActivity.this, NextMemberPro.class);
+            startActivity(intent);
+        });
     }
 }

95f21a2e50c4cfa7310085b866377e9119cf4889
김민주 <sfhketum1231@gmail.com>
2025-05-19 15:40:59 +0900

    feat : 캘린더 반응시 창 전환 기능

--- app/src/main/java/com/example/mobileteamapp/HomeActivity.java
+++ app/src/main/java/com/example/mobileteamapp/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.mobileteamapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +26,7 @@ public class HomeActivity extends AppCompatActivity {
 
         // 로그인된 사용자 ID 받아오기
         memberId = getIntent().getStringExtra("member_id");
+        CalendarView calendarView = findViewById(R.id.calendarView);
 
         // ViewModel 초기화
         memberViewModel = new ViewModelProvider(
@@ -35,6 +38,22 @@ public class HomeActivity extends AppCompatActivity {
         memberViewModel.getAllMembers().observe(this, members -> {
             for (Member m : members) {
                 Log.d("MEMBER_DB", "id=" + m.getMember_id() + ", nickname=" + m.getNickname());
+            }
+        });
+
+
+        //캘린더 부분
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                // 날짜 문자열 생성 (선택사항)
+                String date = year + "/" + (month + 1) + "/" + dayOfMonth;
+
+                Intent intent = new Intent(HomeActivity.this, dream_look_screen_Activity_1.class);
+                intent.putExtra("year", year);
+                intent.putExtra("month", month + 1);  // 월은 0부터 시작하므로 +1
+                intent.putExtra("day", dayOfMonth);
+                startActivity(intent);
             }
         });
     }
